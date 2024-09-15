a8a484c4f636e33a735bc503b64368d828f8eef4
ngoctong-3107 <ngoctong3107@gmail.com>
2024-09-15 23:06:07 +0700

    protect route profile

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import CartView from "@/views/CartView.vue";
 import HomeView from "@/views/HomeView.vue";
 import { default as ProductDetailsView } from "@/views/ProductDetailsView.vue";
 import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "../stores/auth";
 import ProfileView from "../views/ProfileView.vue";
 
 const router = createRouter({
@@ -30,5 +31,14 @@ const router = createRouter({
     },
   ],
 });
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore(); // Lấy store quản lý trạng thái đăng nhập
 
+  // Kiểm tra nếu route yêu cầu đăng nhập và người dùng chưa đăng nhập
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next("/"); // Chuyển hướng về trang chủ (hoặc trang đăng nhập)
+  } else {
+    next(); // Tiếp tục điều hướng nếu đã đăng nhập hoặc route không yêu cầu đăng nhập
+  }
+});
 export default router;
